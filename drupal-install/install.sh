e0da49e6920dafa1138086267add9accea00d6f2
Leonard O' Sullivan <leonard@firstnode.com.au>
2016-12-12 18:20:38 +1000

    Got site install cranking.

--- drupal-install/install.sh
+++ drupal-install/install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./vendor/drush/drush/drush -y --root=/var/www/web si --db-url=mysql://drupal-user:drupal-password@drupal-mysql/drupl-db
+./vendor/drush/drush/drush -y --root=/var/www/web si --db-url=mysql://drupal-user:drupal-password@drupal-mysql/drupal-db
